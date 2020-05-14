a833d0176e77910c899faf3463649bdeba2feb21
mitchell.n@outlook.com <Llechtim19961>
2020-05-14 11:00:55 +0200

    Home opnieuw...

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/about', function () {
-    return view('about');
+Route::get('/home', function () {
+    return view('home');
 
 });
